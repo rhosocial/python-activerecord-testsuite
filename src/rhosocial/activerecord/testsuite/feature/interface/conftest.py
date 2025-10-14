f8a3cf795931d9f8e370dcffd365d91e90c07254
Zhao vistart <i@vistart.me>
2025-10-15 01:13:58 +0800

    refactor: Streamline test structure and fix implementation issues

---
+++ src/rhosocial/activerecord/testsuite/feature/interface/conftest.py
@@ -0,0 +1,7 @@
+﻿# src/rhosocial/activerecord/testsuite/feature/interface/conftest.py
+"""pytest configuration for interface tests.
+
+This file provides pytest fixtures for the interface feature tests.
+Even though it might seem empty, it's an important part of the test
+discovery process.
+"""
