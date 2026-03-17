c6541b5c447e017121652cfcc17c68929d22999e
zhao vistart <i@vistart.me>
2026-03-17 18:44:24 +0800

    refactor(utils): reorganize utils module structure
    Rename utils.py to utils/common.py to avoid conflict with the utils/
    package directory. This allows proper module organization with
    fixture_selector.py and future utility modules.
    
    Changes:
    - Move utils.py content to utils/common.py
    - Update utils/__init__.py to export all functions from common.py
    - No functional changes to the API

--- src/rhosocial/activerecord/testsuite/utils/__init__.py
+++ src/rhosocial/activerecord/testsuite/utils/__init__.py
@@ -2,9 +2,80 @@
 """
 Testsuite utility functions.
 
-This package provides utility functions for the testsuite.
+This package provides utility functions for the testsuite:
+- Protocol checking decorators for marking test requirements
+- Runtime protocol validation functions
+- Convenience decorators for common protocol requirements
+- Fixture selection utilities
 """
+
+from .common import (
+    # Backend access
+    get_current_backend,
+    get_backend_from_model,
+
+    # Protocol checking
+    skip_test_if_protocol_unsupported,
+
+    # Decorators
+    requires_protocol,
+
+    # Convenience decorators
+    requires_window_functions,
+    requires_advanced_grouping,
+    requires_cube,
+    requires_rollup,
+    requires_grouping_sets,
+    requires_cte,
+    requires_recursive_cte,
+    requires_json_operations,
+    requires_inner_join,
+    requires_left_join,
+    requires_cross_join,
+    requires_natural_join,
+    requires_right_join,
+    requires_full_join,
+    requires_lateral_joins,
+    requires_upsert,
+    requires_array_operations,
+    requires_returning_clause,
+    requires_set_operations,
+)
 
 from .fixture_selector import select_fixture
 
-__all__ = ['select_fixture']
+__all__ = [
+    # Backend access
+    'get_current_backend',
+    'get_backend_from_model',
+
+    # Protocol checking
+    'skip_test_if_protocol_unsupported',
+
+    # Decorators
+    'requires_protocol',
+
+    # Convenience decorators
+    'requires_window_functions',
+    'requires_advanced_grouping',
+    'requires_cube',
+    'requires_rollup',
+    'requires_grouping_sets',
+    'requires_cte',
+    'requires_recursive_cte',
+    'requires_json_operations',
+    'requires_inner_join',
+    'requires_left_join',
+    'requires_cross_join',
+    'requires_natural_join',
+    'requires_right_join',
+    'requires_full_join',
+    'requires_lateral_joins',
+    'requires_upsert',
+    'requires_array_operations',
+    'requires_returning_clause',
+    'requires_set_operations',
+
+    # Fixture selection
+    'select_fixture',
+]
